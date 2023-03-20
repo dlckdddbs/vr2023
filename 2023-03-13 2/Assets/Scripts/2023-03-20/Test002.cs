2457d00cc88f41219e8ff3bb0fc5b37790db6c91
dlckddbs <booms5@naver.com>
2023-03-20 11:54:49 +0900

    변수 선언 테스트 0320

---
+++ 2023-03-13 2/Assets/Scripts/2023-03-20/Test002.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test002 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+
+        int n1 = 9;
+        int n2 = 9;
+        int answer2;
+        answer2 = n1 + n2;
+        Debug.Log(answer2);
+
+        answer2 += 5;
+        Debug.Log(answer2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
